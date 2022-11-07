763f034d536dc5793fdddf101b97713cabc2ba46
Alex Adriano Cita <acita@uolinc.com>
2022-11-07 09:56:34 -0300

    upload de arquivo

---
+++ upload/upload.php
@@ -0,0 +1,29 @@
+<!-- carrega arquivo para ser enviado -->
+<form method="POST" enctype="multipart/form-data">
+
+<input type="file" name= "fileUpload">
+<button type="submit">Send</button>
+
+</form>
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+    $file = $_FILES["fileUpload"];
+    if($file["erros"]){
+        throw new Exception("Error: ". $file["error"]);
+
+    }
+    $dirUploads = "uploads";
+    if(!is_dir($dirUploads)){
+        mkdir($dirUploads);
+    }
+
+    if(move_uploaded_file($file["tmp_name"], $dirUploads . DIRECTORY_SEPARATOR . $file["name"])){
+        echo "Upload realizado com sucesso!";
+
+    }else{
+        throw new Exception("Não foi possível realizar upload");
+    }
+}
+
