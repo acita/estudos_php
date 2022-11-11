1b7e59d5cbc5394cc07039883ad195cfc1d2f2c1
Alex Adriano Cita <acita@uolinc.com>
2022-11-11 11:10:55 -0300

    mcrypt

---
+++ criptografia/cripto.php
@@ -0,0 +1,29 @@
+<?php
+
+//até php7.0
+// $data = [
+//     "nome" => "alex"
+// ];
+
+// define('SECRET', pack('a16', 'senha'));
+
+// $mscrypt = mcrypt_encrypt(
+//     MCRYPT_RIJNDAEL_128,
+//     SECRET,
+//     json_encode($data),
+//     MCRYPT_MODE_ECB
+// );
+
+// $encripted = base64_encode($mscrypt);
+
+// $string = mcrypt_decrypt(
+//     MCRYPT_RIJNDAEL_128,
+//     SECRET,
+//     base64_decode($encripted),
+//     MCRYPT_MODE_ECB
+// );
+
+// echo $encripted;
+
+// a partir do PHP7.1.0 SSL
+
